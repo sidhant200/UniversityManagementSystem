af47b0d72c5293ca1bebb7cb5b857489fe67fee8
Sidhant Bhandari <bhandarisidhant71@gmail.com>
2023-07-26 13:00:56 +0530

    AddStudent.java

---
+++ src/university/management/system/AddStudent.java
@@ -0,0 +1,17 @@
+package university.management.system;
+
+import javax.swing.*;
+
+public class AddStudent extends JFrame {
+
+    AddStudent(){
+        setSize(900 , 600);
+        setLayout(null);
+        setLocation(150 , 60);
+        setVisible(true);
+    }
+
+    public static void main(String[] args) {
+        new AddStudent();
+    }
+}
